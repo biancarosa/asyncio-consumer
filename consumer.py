4fd93295bdfa8095f3e21a32e3b8b6f91da8147f
Bianca Rosa <me@biancarosa.com.br>
2017-05-12 00:08:30 -0300

    Tenta fazer funcionar...

--- consumer.py
+++ consumer.py
@@ -8,6 +8,21 @@ class Consumer(object):
         self.connection = None
         self.channel = None
 
+    @asyncio.coroutine
+    def start(self, loop):
+        try:
+            yield from self.setup()
+            yield from self.consume()
+        except Exception:
+            yield from asyncio.sleep(1)
+            loop.create_task(self.start(loop))
+
+    @asyncio.coroutine
+    def setup(self):
+        self.connection = yield from asynqp.connect('localhost', 5672, username='guest', password='guest')
+        self.channel = yield from self.connection.open_channel()
+        print("Connected")
+
     def callback(self, msg):
         print("Got message")
         print(msg)
@@ -15,30 +30,26 @@ class Consumer(object):
 
     @asyncio.coroutine
     def consume(self):
-        """
-        Sends a 'hello world' message and then reads it from the queue.
-        """
-        print("Closing")
-        self.connection = yield from asynqp.connect('localhost', 5672, username='guest', password='guest')
-        self.channel = yield from self.connection.open_channel()
         exchange = yield from self.channel.declare_exchange('test.exchange', 'direct')
         queue = yield from self.channel.declare_queue('test.queue')
         print("Consuming")
-        yield from queue.consume(self.callback)
+        consumer = yield from queue.consume(self.callback)
+        return consumer
 
     @asyncio.coroutine
     def close(self):
         print("Closing")
         yield from self.channel.close()
         yield from self.connection.close()
+        print("Shutdown complete ...")
+
+    def exception_handler(self, loop, context):
+        close_task = loop.create_task(self.close())
+        asyncio.wait_for(close_task, None)
 
 if __name__ == "__main__":
     consumer = Consumer()
     loop = asyncio.get_event_loop()
-    try:
-        loop.create_task(consumer.consume())
-        loop.run_forever()
-    finally:
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(consumer.close())
-        loop.close()
+    loop.set_exception_handler(consumer.exception_handler)
+    loop.create_task(consumer.start(loop))
+    loop.run_forever()
